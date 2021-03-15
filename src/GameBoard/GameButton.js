86339670bc3cbd0262d1ac5a31ce088f2813b579
James Atkinson <CSAtkinson8588@gmail.com>
2021-03-15 15:18:05 -0400

    More refactoring to figure out excess calls

--- src/GameBoard/GameButton.js
+++ src/GameBoard/GameButton.js
@@ -1,2 +1,23 @@
+import React from 'react'
+import classes from './Gameboard.module.css'
 
+const name = (props) => {
+
+    const tester = new Array(3).fill(0).map(() => new Array(3).fill(0))
+    let num = 0;
+    for(var i = 0; i < 3; i++){
+        for (var j = 0; j <3; j++){
+            tester[i][j] = <button  className={classes.Button}></button>
+            num++
+        }
+    } 
+    return(
+        <div className={classes.emptyDiv}>
+            {tester}
+        </div>
+    )
+    
+};
+
+export default name
 
--- src/GameBoard/Gameboard.js
+++ src/GameBoard/Gameboard.js
@@ -1,42 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import classes from './Gameboard.module.css'
+import TttButton from './GameButton'
 
+const GameBoard = (props) => { 
+    
 
+    const symbolChange = () => {
 
-
-
-const GameBoard = (props) => {
-   
-    const tester = new Array(3).fill(0).map(() => new Array(3).fill(0))
-    const populate = () => {         
-        let num = 0;
-        for(var i = 0; i < 3; i++){
-            for (var j = 0; j <3; j++){
-                tester[i][j] = <button key={num} onClick={()=> setSymbol("O")} className={classes.Button}>X</button>
-                num++
-            }
-        }        
     }
-
-    const [symbol, setSymbol] = useState("X");
-    const [turn, setTurn] = useState(1)
-    const [button, setButton] = useState([])
-    populate();
-    console.log(tester)
-
     
     
-
-    // useEffect(() => {
-        
-    // }, [])
 
     return(        
         <div className={classes.outerDiv}>
             <div className={classes.emptyDiv}></div>
-            <div className={classes.emptyDiv}>
-            {tester}
-            </div>            
+            <TttButton click={symbolChange}/>            
             <div className={classes.emptyDiv}></div>
         </div>
     ) 
@@ -46,5 +24,5 @@ const GameBoard = (props) => {
 export default GameBoard
 
 
-//
+
 
