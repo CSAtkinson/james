944cf59a970df59a8d379326c44ca8b4036a7735
James Atkinson <CSAtkinson8588@gmail.com>
2021-03-19 10:33:14 -0400

    game fully functional before working on multiplayer

--- src/GameBoard/Gameboard.js
+++ src/GameBoard/Gameboard.js
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from 'react'
 import classes from './Gameboard.module.css'
 import GameButton from './GameButton'
 
-const GameBoard = ({gameButtons, onClick}) => ( 
-    
-        <div>
-            <div className={classes.emptyDiv}></div>
+const GameBoard = ({gameButtons, onClick, turn, winner}) => {
+
+    let turnWinner = (<p>Player Turn: {turn}</p>)
+
+    if(winner){
+        turnWinner = <p>WINNER!!!</p>
+    }  
+
+    return(
+        <div className={classes.outerDiv}>
+            <div className={classes.emptyDiv}> {turnWinner} </div>
             <div className={classes.emptyDiv}>
             {gameButtons.map((gameButton, i) => (
                 <GameButton key={i} value={gameButton} onClick={()=> onClick(i)} />
             ))}           
             </div>
             <div className={classes.emptyDiv}></div>
-            
         </div>
-        
-)
-
+    )
+}
 
 export default GameBoard
 
--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -1,67 +1,88 @@
-import React, { Component, useEffect, useState } from 'react'
+import React, { Component } from 'react'
 import GameBoard from '../GameBoard/Gameboard'
 import Auxillary from '../hoc/Auxillary/Auxillary'
 
-const TicTacToe = () =>{
-    const [gameBoard, setGameboard] = useState(Array(9).fill(null))
-    const [turn, setTurn] = useState(1)
-     
-    const handleClick = (i) => {
-        const boardCopy = [...gameBoard]
-        if( turn === 1){
+class TicTacToe extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          gameBoard: Array(9).fill(null),
+          turn: 1,
+          winner: false
+        };
+      }
+
+      handleClick = (i) => {
+        const boardCopy = [...this.state.gameBoard]
+        if (this.determineWinner()){return}
+        if( this.state.turn === 1){
             boardCopy[i] = 'X'
-            setTurn(2)            
+            this.setState({turn: 2})            
         }
         else {
             boardCopy[i] = 'O'
-            setTurn(1)            
+            this.setState({turn: 1})                     
         }
-        setGameboard(boardCopy)
+        this.setState({gameBoard: boardCopy}, () => this.determineWinner())        
+    }
+
+    determineWinner(){
+        if ((this.state.gameBoard[0] === 'X' && this.state.gameBoard[1] === 'X' && this.state.gameBoard[2] === 'X') || (this.state.gameBoard[0] === 'O' && this.state.gameBoard[1] === 'O' && this.state.gameBoard[2] === 'O')){
+            console.log("WINNER!!!")
+            this.setState({winner: true})
+            return true;
+        }
+        else if ((this.state.gameBoard[0] === 'X' && this.state.gameBoard[3] === 'X' && this.state.gameBoard[6] === 'X') || (this.state.gameBoard[0] === 'O' && this.state.gameBoard[3] === 'O' && this.state.gameBoard[6] === 'O')){
+            this.setState({winner: true})
+            return true
+        }
+        else if ((this.state.gameBoard[0] === 'X' && this.state.gameBoard[4] === 'X' && this.state.gameBoard[8] === 'X') || (this.state.gameBoard[0] === 'OX' && this.state.gameBoard[4] === 'O' && this.state.gameBoard[8] === 'O')){
+            this.setState({winner: true})
+            return true
+        }
+        else if ((this.state.gameBoard[1] === 'X' && this.state.gameBoard[4] === 'X' && this.state.gameBoard[7] === 'X') || (this.state.gameBoard[1] === 'O' && this.state.gameBoard[4] === 'O' && this.state.gameBoard[7] === 'O')){
+            this.setState({winner: true})
+            return true
+        }
+        else if ((this.state.gameBoard[2] === 'X' && this.state.gameBoard[4] === 'X' && this.state.gameBoard[6] === 'X') || (this.state.gameBoard[2] === 'O' && this.state.gameBoard[4] === 'O' && this.state.gameBoard[6] === 'O')){
+            this.setState({winner: true})
+            return true
+        }
+        else if ((this.state.gameBoard[2] === 'X' && this.state.gameBoard[5] === 'X' && this.state.gameBoard[8] === 'X') || (this.state.gameBoard[2] === 'O' && this.state.gameBoard[5] === 'O' && this.state.gameBoard[8] === 'O')){
+            this.setState({winner: true})
+            return true
+        }
+        else if ((this.state.gameBoard[3] === 'X' && this.state.gameBoard[4] === 'X' && this.state.gameBoard[5] === 'X') || (this.state.gameBoard[3] === 'O' && this.state.gameBoard[4] === 'O' && this.state.gameBoard[5] === 'O')){
+            this.setState({winner: true})
+            return true
+        }
+        else if ((this.state.gameBoard[6] === 'X' && this.state.gameBoard[7] === 'X' && this.state.gameBoard[8] === 'X') || (this.state.gameBoard[6] === 'O' && this.state.gameBoard[7] === 'O' && this.state.gameBoard[8] === 'O')){
+            this.setState({winner: true})
+            return true
+        }
+        return false;
         
     }
 
-    const determineWinner = () => {
-        if ((gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') || (gameBoard[0] === 'O' && gameBoard[1] === 'O' && gameBoard[2] === 'O')){
-           console.log("We have a winner!")
-           setGameboard(Array(9).fill(null))
-        }
-        else if ((gameBoard[0] === 'X' && gameBoard[3] === 'X' && gameBoard[6] === 'X') || (gameBoard[0] === 'O' && gameBoard[3] === 'O' && gameBoard[6] === 'O')){
-            console.log("We have a winner!")
-            setGameboard(Array(9).fill(null))
-        }
-        else if ((gameBoard[0] === 'X' && gameBoard[4] === 'X' && gameBoard[8] === 'X') || (gameBoard[0] === 'OX' && gameBoard[4] === 'O' && gameBoard[8] === 'O')){
-            console.log("We have a winner!")
-            setGameboard(Array(9).fill(null))
-        }
-        else if ((gameBoard[1] === 'X' && gameBoard[4] === 'X' && gameBoard[7] === 'X') || (gameBoard[1] === 'O' && gameBoard[4] === 'O' && gameBoard[7] === 'O')){
-            console.log("We have a winner!")
-            setGameboard(Array(9).fill(null))
-        }
-        else if ((gameBoard[2] === 'X' && gameBoard[4] === 'X' && gameBoard[6] === 'X') || (gameBoard[2] === 'O' && gameBoard[4] === 'O' && gameBoard[6] === 'O')){
-            console.log("We have a winner!")
-            setGameboard(Array(9).fill(null))
-        }
-        else if ((gameBoard[2] === 'X' && gameBoard[5] === 'X' && gameBoard[8] === 'X') || (gameBoard[2] === 'O' && gameBoard[5] === 'O' && gameBoard[8] === 'O')){
-            console.log("We have a winner!")
-            setGameboard(Array(9).fill(null))
-        }
-        else if ((gameBoard[3] === 'X' && gameBoard[4] === 'X' && gameBoard[5] === 'X') || (gameBoard[3] === 'O' && gameBoard[4] === 'O' && gameBoard[5] === 'O')){
-            console.log("We have a winner!")
-            setGameboard(Array(9).fill(null))
-        }
-        else if ((gameBoard[6] === 'X' && gameBoard[7] === 'X' && gameBoard[8] === 'X') || (gameBoard[6] === 'O' && gameBoard[7] === 'O' && gameBoard[8] === 'O')){
-            console.log("We have a winner!")
-            setGameboard(Array(9).fill(null))
-        }
+
+    render(){
+        return(
+            <Auxillary>
+                <GameBoard winner={this.state.winner} turn={this.state.turn} gameButtons={this.state.gameBoard} onClick={this.handleClick}/>             
+            </Auxillary>
+        )
+
         
     }
 
+    
+     
+    
 
-    return(
-        <Auxillary>
-            <GameBoard gameButtons={gameBoard} onClick={handleClick}/>             
-        </Auxillary>
-    );
+    
+
+
+    
     
 };
 
